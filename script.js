1435bdaa536d85ab4b7133d8cdcb9e925cea29f9
FrancisEtham <francisetham01@gmail.com>
2022-06-08 01:48:23 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById('btn');
+const jokeContainer = document.getElementById('joke');
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
+
+let getJoke = () => {
+    jokeContainer.classList.remove("fade")
+    fetch(url)
+    .then(data => data.json())
+    .then(data => {
+        jokeContainer.textContent = `${item.joke}`;
+        jokeContainer.classList.add("fade");
+    });
+}
+
+btn.addEventListener("click",getJoke)
+getJoke()
